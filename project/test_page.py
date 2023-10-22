1dfa95d2c4a2ee6c7b926b9c7572b043879685e0
Alekbs <aleksandrb436@gmail.com>
2023-10-22 17:12:46 +0300

    db

--- project/test_page.py
+++ project/test_page.py
@@ -1,19 +1,59 @@
 import flet as ft
-
+import sqlite3
 
 def main(page: ft.Page):
     page.title = "Routes Example"
+    text_email = ft.TextField(label="email")
+    #создаем бд
+    connection = sqlite3.connect('chat.db')
+    cursor = connection.cursor()
 
+    #создадим таблицу user
+    cursor.execute("""
+    CREATE TABLE if NOT EXISTS Users(
+    id INTEGER PRIMARY KEY,
+    login TEXT NOT NULL,
+    password TEXT NOT NULL,
+    email TEXT NOT NULL               
+    )
+    """)
+        
+    #сохраняем и закрываем соединение
+    connection.commit()
+    print(cursor.execute('SELECT * FROM Users').fetchall())
+
+    connection.close()
     def route_change(route):
         page.views.clear()
+        alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+        vertical_alignment=ft.CrossAxisAlignment.CENTER,
+
         page.views.append(
             ft.View(
+                
                 "/login",
                 [
                     ft.AppBar(title=ft.Text("Вход"), bgcolor=ft.colors.SURFACE_VARIANT),
-                    ft.TextField(label="login"),
-                    ft.TextField(label="Password"),
                     ft.Row(
+                        vertical_alignment=ft.MainAxisAlignment.CENTER,
+
+                        alignment=ft.MainAxisAlignment.CENTER,
+                        controls=[
+                            ft.TextField(label="login", width=250),
+                        ]
+                    ),
+                    ft.Row(
+                        vertical_alignment=ft.MainAxisAlignment.CENTER,
+
+                        alignment=ft.MainAxisAlignment.CENTER,
+                        controls=[
+                            ft.TextField(label="Password", width=250),
+                        ]
+                    ),
+                    ft.Row(
+                        
+                        alignment=ft.MainAxisAlignment.CENTER,
+                        vertical_alignment=ft.MainAxisAlignment.CENTER,
                         controls=[
                             ft.ElevatedButton(
                                 "Log in", on_click=lambda _: page.go("/store")
@@ -23,7 +63,13 @@ def main(page: ft.Page):
                             ),
                         ]
                     ),
-                    ft.ElevatedButton("About us", on_click=lambda _: page.go("/about")),
+                    ft.Row(
+                        alignment=ft.MainAxisAlignment.CENTER,
+                        controls=[
+                            ft.ElevatedButton("About us", on_click=lambda _: page.go("/about")),
+                        ]
+                    )
+                    
                 ],
             )
         )
@@ -32,14 +78,36 @@ def main(page: ft.Page):
                 ft.View(
                     "/signin",
                     [
+                        
                         ft.AppBar(title=ft.Text("Регистрация"), bgcolor=ft.colors.SURFACE_VARIANT),
-                        ft.AppBar(title=ft.Text("Вход"), bgcolor=ft.colors.SURFACE_VARIANT),
-                        ft.TextField(label="email"),
-                        ft.TextField(label="login"),
-                        ft.TextField(label="Password"),
-                        ft.ElevatedButton(
-                            "Confirm", on_click=lambda _: page.go("/login")
+                        ft.Row(
+                            alignment=ft.MainAxisAlignment.CENTER,
+                            controls=[
+                                text_email,
+                            ]
                         ),
+                        ft.Row(
+                            alignment=ft.MainAxisAlignment.CENTER,
+                            controls=[
+                                ft.TextField(label="login"),
+                            ]
+                        ),
+                        ft.Row(
+                            alignment=ft.MainAxisAlignment.CENTER,
+                            controls=[
+                                ft.TextField(label="Password"),
+                            ]
+                        ),
+                        ft.Row(
+                            alignment=ft.MainAxisAlignment.CENTER,
+                            controls=[
+                                ft.ElevatedButton(
+                                    "Confirm", on_click=add_user
+                                ),
+                            ]
+
+                        )
+                        
                     ],
                 )
             )
@@ -74,11 +142,32 @@ def main(page: ft.Page):
         page.views.pop()
         top_view = page.views[-1]
         page.go(top_view.route)
+    
+    def add_user(e):
+        #создаем бд
+        connection = sqlite3.connect('chat.db')
+        cursor = connection.cursor()
+
+        #добавляем пользователя
+        cursor.execute('INSERT INTO Users (login, password, email) VALUES (?,?,?)', ('','',text_email.value))
+            
+        #сохраняем и закрываем соединение
+        connection.commit()
+        print(cursor.execute('SELECT * FROM Users').fetchall())
+
+        connection.close()
+        print(text_email.value)
+        
+
+        page.go("/login")
+
 
     page.on_route_change = route_change
     page.on_view_pop = view_pop
     page.go(page.route)
     page.window_center()
+    alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+    vertical_alignment=ft.CrossAxisAlignment.CENTER,
 
 
 ft.app(target=main)
